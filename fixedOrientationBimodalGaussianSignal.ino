d782cb3d121a835e247117cd84ab4e946bba3629
mrdevlin6 <mrdevlin6@gmail.com>
2023-05-31 11:39:32 -0700

    Add files via upload

---
+++ fixedOrientationBimodalGaussianSignal.ino
@@ -0,0 +1,243 @@
+#include <SparkFun_TB6612.h>
+#include <movingAvg.h>
+#include <Gaussian.h>
+//#include <SPI.h>
+
+const int diodes_idx[8] = { A0, A1, A2, A3, A4, A5, A6, A7 };
+int diodes[8];     // stores raw diode readings
+String text = "";  // dummy serial print variable
+// int headPin = -1;
+
+
+int avg[8];  //smoothing buffer
+int avgBuff = 25;
+movingAvg s1(avgBuff);
+movingAvg s2(avgBuff);
+movingAvg s3(avgBuff);
+movingAvg s4(avgBuff);
+movingAvg s5(avgBuff);
+movingAvg s6(avgBuff);
+movingAvg s7(avgBuff);
+movingAvg s8(avgBuff);
+
+#define AIN1 2
+#define BIN1 7
+#define AIN2 4
+#define BIN2 8
+#define PWMA 5
+#define PWMB 6
+#define STBY 9  //3 10
+#define ERM_PIN 10
+const int offsetA = 1;
+const int offsetB = 1;
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+int minOut[2] = { 0, 0 };
+int spd = 0;
+void setSpins(int headPin, int spd);
+void arrMin(int (&avg)[8], int (&minOut)[2]);  //was diodes
+int polarizeHead(int spd);                     //not spd but just need a 0
+int fluctuationSignal(int centerline, int fluctuationAvg, Gaussian g1, int sigma);
+bool program = true;
+int index = 0;
+int vibArr[11] = { 255, 0, 0, 0, 0, 0, 0, 175, 200, 225, 250 };
+int driveArr[11] = { 0, 0, 0, 0, 0, 0, 150, 175, 200, 225, 250 };
+int randSpeed = 0;
+
+
+int n = -2;  //if it's -1 you divide by zero idiot
+
+
+bool flip = false;
+int coin = 1;
+double coinRand = 0;
+int headPin = int(random(0, 7));  //gives a randomNumber
+
+//------------------------------------------------------------------------
+
+int centerLine = 110;
+int max_amp = 60;
+
+int high_dt = 2;// should be 470 TODO
+int low_dt = 2;  // should be 470 changes per experiment TODO
+
+int dt_counter = 0;
+
+//------------------------------------------------------------------------
+unsigned long start = millis();
+double randNumber = 0;
+int phase_offset = 0;
+
+
+int sigma = 5;
+Gaussian g1 = Gaussian(0, sigma*sigma/4); //was 0, 20 mean, variance. std = 6, bimodal at 2sigma . cut variance in half so two sigma is actually 5
+int var = fluctuationSignal(centerLine, max_amp, g1, sigma);
+
+
+void setup() {
+
+  TCCR0B = TCCR0B & B11111000 | B00000001;  //62.5khz
+  //TCCR0B = TCCR0B & B11111000 | B00000010; 7k ish
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(13, OUTPUT);
+  for (int i = 0; i < 8; i++) {
+    pinMode(diodes_idx[i], INPUT);  // initiate pinModes
+  }
+  Serial.begin(115200);  // usually 500000 115200
+  // Serial.println("Start");
+  analogWrite(ERM_PIN, 0);
+  s1.begin();
+  s2.begin();
+  s3.begin();
+  s4.begin();
+  s5.begin();
+  s6.begin();
+  s7.begin();
+  s8.begin();
+  //    delay(67*10000);
+  randomSeed(analogRead(3));
+
+  analogWrite(ERM_PIN, 0);
+  coinRand = random(0.01, 100) / 100.00;
+  dt_counter = int(random(300, 800));
+  // phase_offset = int(random(100, 300)); TODO
+
+  if (coinRand > 0.5) {
+    coin = -1;
+  }
+  headPin = int(random(0, 7));
+}
+
+
+// double var = g1.random();
+
+void loop() {
+  // scan all pins and record current state
+  for (int i = 0; i < 8; i++) {
+    diodes[i] = analogRead(diodes_idx[i]);
+    if (i == 0) {
+      avg[i] = s1.reading(diodes[i]);
+    } else if (i == 1) {
+      avg[i] = s2.reading(diodes[i]);
+    } else if (i == 2) {
+      avg[i] = s3.reading(diodes[i]);
+    } else if (i == 3) {
+      avg[i] = s4.reading(diodes[i]);
+    } else if (i == 4) {
+      avg[i] = s5.reading(diodes[i]);
+    } else if (i == 5) {
+      avg[i] = s6.reading(diodes[i]);
+    } else if (i == 6) {
+      avg[i] = s7.reading(diodes[i]);
+    } else if (i == 7) {
+      avg[i] = s8.reading(diodes[i]);
+    }
+  }
+
+  int counter = 0;
+  int counter2 = 0;
+  for (int i = 0; i < 8; i++) {
+    if (avg[i] > 600) {
+      counter++;
+      //      Serial.println(avg[i]);
+    }
+  }
+
+  arrMin(avg, minOut);  //was diodes
+  int index = minOut[0];
+  
+  // headPin = polarizeHead(index);
+
+  
+  //  Serial.println(millis());
+  if (counter < 2) { //>2 TODO
+    if (dt_counter < (high_dt)) {
+        setSpins(headPin, round(centerLine + max_amp + var));
+        Serial.println(round(centerLine + max_amp + var));
+        dt_counter++;
+      } 
+      else if (dt_counter >= high_dt & dt_counter < (high_dt+low_dt)) {
+        setSpins(headPin, round(centerLine - max_amp - var));
+        Serial.println(round(centerLine - max_amp - var));
+        dt_counter++;
+      } else if (dt_counter >= (high_dt + low_dt)) {
+        dt_counter = 0;
+        var = fluctuationSignal(centerLine, max_amp, g1, sigma);
+        // phase_offset = int(random(100, 300)); TODO
+
+      }
+
+  } else {
+    setSpins(headPin, 0);
+    
+  }
+  
+}
+
+//----------------------------------------------------------------
+//                           WRAPPERS
+//----------------------------------------------------------------
+
+
+void setSpins(int headPin, int spd) {
+  // camer spd = 100;
+  if (spd < 0 || spd > 255) {
+    spd = 0;
+  }
+  if (headPin == 0 || headPin == 4) {
+    motor1.drive(spd, 10);
+    motor2.drive(spd, 10);
+  } else if (headPin == 1 || headPin == 5) {
+    motor1.drive(-1 * spd, 10);
+    motor2.drive(spd, 10);
+  } else if (headPin == 2 || headPin == 6) {
+    motor1.drive(-1 * spd, 10);
+    motor2.drive(-1 * spd, 10);
+  } else if (headPin == 3 || headPin == 7) {
+    motor1.drive(spd, 10);
+    motor2.drive(-1 * spd, 10);
+  } else {
+    motor1.drive(0, 10);
+    motor2.drive(0, 10);
+  }
+}
+
+void arrMin(int (&diodes)[8], int (&minOut)[2]) {  //need wrapper for full array max/min finding using native functions
+  int j = 0;
+  for (int i = 0; i < 8; i++) {
+    if (i == 0) {
+      minOut[0] = i;
+      minOut[1] = diodes[0];
+    } else {
+      if (diodes[i] < minOut[1]) {
+        minOut[0] = i;
+        minOut[1] = diodes[i];
+      }
+    }
+  }
+}
+
+int fluctuationSignal(int centerline, int fluctuationAvg, Gaussian g1, int sigma) {  //edit this for signal. this one is bimodal
+  
+  //was 0, 20 mean, variance. std = 6, bimodal at 2sigma
+  double var = g1.random();
+  
+  if (abs(var) > sigma) {
+    if (var > 0) {
+      var = sigma;
+    } else {
+      var = -1*sigma;
+    }
+  }
+  // Serial.println(var);
+  coinRand = random(0.01, 100) / 100.00;
+  if (coinRand > 0.5) {
+    coin = -1;
+  } else {
+    coin = 1;
+  }
+  return round(coin*(2*sigma + var));
+
+}
